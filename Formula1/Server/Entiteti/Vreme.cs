b14839fb24d935794dffe9042ee89b09dc74ccdd
Nole <daki.nole@gmail.com>
2026-02-09 02:35:48 +0100

    SERVER - UI

---
+++ Formula1/Server/Entiteti/Vreme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Entiteti
+{
+    public class Vreme
+    {
+        public string Vozac { get; set; } = "";
+        public double VremeKruga { get; set; } = 0.0;
+        public bool Najbrze { get; set; } = false;
+    }
+}
